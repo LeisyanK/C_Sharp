684146ee26081824c2234001cb436c0d2e0ecea8
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-06 20:02:35 +0500

    Example003 Sum project

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example003_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 3;
+            int numberB = 5;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+        }
+    }
+}
