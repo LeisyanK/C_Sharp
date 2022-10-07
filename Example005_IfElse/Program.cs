e63f924481490484cf30a43389d804c2f81785ea
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 09:33:05 +0500

    Example005 if else project

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example005_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string username = Console.ReadLine();
+
+            if (username.ToLower() == "маша")
+            {
+                Console.WriteLine("Ура, это же МАША!");
+            }
+            else
+            {
+                Console.Write("Привет, ");
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
