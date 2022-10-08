f9315cb682a002a43d474468d5846cc3e80c61be
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-08 22:57:06 +0500

    Example009 Intro Array

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example009_IntroArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Max(int arg1, int arg2, int arg3)
+            {
+                int result = arg1;
+                if (arg2 > result) result = arg2;
+                if (arg3 > result) result = arg3;
+                return result;
+            };
+
+            int[] array = {74, 9, 479, 38, 83, 29, 4, 59, 63};
+
+            int max = Max(
+                Max(array[0], array[1], array[2]),
+                Max(array[3], array[4], array[5]),
+                Max(array[6], array[7], array[8])
+            );
+            Console.WriteLine(max);
+        }
+    }
+}
