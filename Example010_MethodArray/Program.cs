d2ae836ad9c6ca591f73b7c6fa32d8b8016a7b02
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-10 00:13:52 +0500

    Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example010_MethodArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] array = {80, 45, 38, 15, 20, 63};
+           int n = array.Length;
+           int find = 38;
+           int index = 0;
+
+           while (index < n)
+           {
+            if (array[index] == find)
+            {
+                Console.WriteLine(index);
+                break;
+            }
+            index++;
+           }
+        }
+    }
+}
